c9e40110e2fd1061ac5c405d51c21baa3a2a1099
Scott <Scotthall1983@hotmail.com>
2019-09-06 10:51:12 +0100

    Imported AuthContext and a PrivateRoute to App.js. Added an AuthContext.Provider hook that returns false when authentication is provided. In PrivateRoute.js used the hook to pull value stored in AuthContext. This now only allows access to home page, refactor required later.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import Private from "./PrivateRoute";
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
+import { AuthContext } from "./context/auth";
 
 function App(props) {
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home Page</Link>
-          </li>
-          <Link to="/admin">Admin</Link>
-          <li></li>
-        </ul>
-        <Route exact path="/" component={Home} />
-        <Route path="/admin" component={Admin} />
-      </div>
-    </Router>
+    <AuthContext.Provider value={false}>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home Page</Link>
+            </li>
+            <Link to="/admin">Admin</Link>
+            <li></li>
+          </ul>
+          <Route exact path="/" component={Home} />
+          <Route path="/admin" component={Admin} />
+        </div>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
+import { useAuth } from "./context/auth";
 
 function PrivateRoute({ component: Component, ...rest }) {
-  return <Route {...rest} render={props => <Component {...props} />} />;
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        isAuthenticated ? <Component {...props} /> : <Redirect to="/" />
+      }
+    />
+  );
 }
 
 export default PrivateRoute;
