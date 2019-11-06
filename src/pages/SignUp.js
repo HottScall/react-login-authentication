21e7e62621c87fad99b37c3b055dabd131d2cbb1
Scott <Scotthall1983@hotmail.com>
2019-11-06 12:42:39 +0000

    Created the same template as LogIn for SignUp bar some minor differences, the path is set to SignUp instead of Log in. Added some additional required credentials, first name, surname and confirm password

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,19 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
 import LogoImg from "../img/RIAM.png";
 import { Card, Logo, Input, Form, Button } from "../components/AuthForm";
+import { useAuth } from "../context/auth";
 
 function SignUp() {
+  const [isSignedUp, setSignedUp] = useState(false);
+  const [IsError, setIsError] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const { setAuthTokens } = useAuth();
+
+  function postLogin() {
+    axios
+      .post("https://www.somePlace.com/auth/signup", {
+        userName,
+        password
+      })
+      .then(result => {
+        if (result.status === 200) {
+          setAuthTokens(result.data);
+          setLoggedIn(true);
+        } else {
+          setIsError(true);
+        }
+      })
+      .catch(e => {
+        setIsError(true);
+      });
+  }
+
+  if (isLoggedIn) {
+    return <Redirect to="/" />;
+  }
+
   return (
     <Card>
       <Logo src={LogoImg} />
       <Form>
-        <Input type="email" placeholder="email" />
-        <Input type="password" placeholder="password" />
-        <Input type="password" placeholder="confirm password" />
+        <Input type="first name" placeholder="First Name" />
+        <Input type="surname" placeholder="Surname" />
+        <Input type="email" placeholder="Email" />
+        <Input type="password" placeholder="Password" />
+        <Input type="password" placeholder="Confirm Password" />
         <Button>Sign Up</Button>
       </Form>
       <Link to="/login">Already have an account?</Link>
+      {isError && <Error>The passwords provided are not the same</Error>}
     </Card>
   );
 }
