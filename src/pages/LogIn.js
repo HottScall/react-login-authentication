389b4107c3d0870628ba82fc3bafa8af5110ed70
Scott <Scotthall1983@hotmail.com>
2019-11-06 11:55:19 +0000

    Added new const variables for LoggedIn, isError, username, password and authTokens. Used an axios call to fr Login and which checks the credentials and redirects to home if logged in. Added onChange events to return statement set to username and password. Added a button for Sign In and an Error message if uname or pword are incorrect.

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,18 +1,66 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import axios from "axios";
 import LogoImg from "../img/RIAM.png";
-import { Card, Logo, Button, Form, Input } from "../components/AuthForm";
+import { Card, Logo, Button, Form, Input, Error } from "../components/AuthForm";
+import { useAuth } from "../context/auth";
 
 function LogIn() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [setAuthTokens] = useAuth();
+
+  function postLogin() {
+    axios
+      .post("https://www.somePlace.com/auth/login", {
+        userName,
+        password
+      })
+      .then(result => {
+        if (result.state === 200) {
+          setAuthTokens(results.data);
+          setLoggedIn(true);
+        } else {
+          setIsError(true);
+        }
+      })
+      .catch(e => {
+        setIsError(true);
+      });
+  }
+  if (isLoggedIn) {
+    return <Redirect to="/" />;
+  }
+
   return (
     <Card>
       <Logo src={LogoImg} />
       <Form>
-        <Input type="email" placeholder="email" />
-        <Input type="password" placeholder="password" />
+        <Input
+          type="email"
+          value={userName}
+          onChange={e => {
+            setUserName(e.target.value);
+          }}
+          placeholder="email"
+        />
+        <Input
+          type="password"
+          value={password}
+          onChange={e => {
+            setPassword(e.target.value);
+          }}
+          placeholder="password"
+        />
+        <Button onClick={postLogin}>Sign In</Button>
       </Form>
 
       <Link to="/signup">Dont have an account?</Link>
+      {isError && (
+        <Error>The username or password provided was incorrect</Error>
+      )}
     </Card>
   );
 }
