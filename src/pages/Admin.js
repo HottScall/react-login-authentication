29d373a1d6ae800955cf9631a89a65e5b4ffee2c
Scott <Scotthall1983@hotmail.com>
2019-11-06 16:04:35 +0000

    Added content for a signout button, but all set tokenauthentication to make sure that tokens are removed when a user logs out.

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Button } from "../component/AuthForm";
+import { useAuth } from "../context/auth";
 
 function Admin(props) {
-  return <div>Admin</div>;
+  const setAuthTokens = useAuth();
+
+  function logOut() {
+    setAuthTokens();
+  }
+  return (
+    <div>
+      <div>Admin Page</div>
+      <Button onClick={logOut}>Log Out</Button>
+    </div>
+  );
 }
 
 export default Admin;
